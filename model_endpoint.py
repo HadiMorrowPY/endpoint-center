48ccc26f83d9bb6006b2c5eeaf15e92630a3293c
HadiMorrowPY <156124281+HadiMorrowPY@users.noreply.github.com>
2024-02-18 00:51:43 -0500

    Update model_endpoint.py

--- model_endpoint.py
+++ model_endpoint.py
@@ -65,8 +65,16 @@ class ModelMiner():
 
         self.tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
+        name = 'mosaicml/mpt-7b-chat'
+        
+        config = transformers.AutoConfig.from_pretrained(name, trust_remote_code=True)
+        config.attn_config['attn_impl'] = 'triton'
+        config.init_device = 'cuda:0' # For fast initialization directly on GPU!
+        
         self.model = transformers.AutoModelForCausalLM.from_pretrained(
-          'mosaicml/mpt-7b-chat',
+          name,
+          config=config,
+          torch_dtype=torch.bfloat16, # Load model weights in bfloat16
           trust_remote_code=True
         )
         print("model loaded")
